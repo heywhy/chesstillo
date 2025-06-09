f110bcabe25bef2835ef4c1cd201831d7f06cbeb
atanda rasheed <atandarash@gmail.com>
2025-06-09 04:20:03 +0100

    chore(tui): delete modal component

--- tui/include/tui/components.hpp
+++ tui/include/tui/components.hpp
@@ -5,7 +5,6 @@
 #include "component/command_input.hpp"
 #include "component/engine_settings.hpp"
 #include "component/input.hpp"
-#include "component/modal.hpp"
 #include "component/modal_view.hpp"
 #include "component/scroll_view.hpp"
 #include "component/square.hpp"
